35d412920e95882943dd97324b03c13e0bae3b80
湖中沉 <341936031@qq.com>
2025-05-07 00:36:39 +0000

    !2444 header区域增加meta的hook钩子，使插件可向页面添加meta信息 * header区域增加meta的hook钩子，使插件可向页面添加meta信息

--- upload/template/default/common/header_common.php
+++ upload/template/default/common/header_common.php
@@ -15,6 +15,7 @@
 	<meta name="copyright" content="{lang copyright_s}" />
 	<meta name="MSSmartTagsPreventParsing" content="True" />
 	<meta http-equiv="MSThemeCompatible" content="Yes" />
+	<!--{hook/global_meta}-->
 	<base href="{$_G['siteurl']}" />
 	<!--{csstemplate}-->
 	<script type="text/javascript">var STYLEID = '{STYLEID}', STATICURL = '{STATICURL}', IMGDIR = '{IMGDIR}', VERHASH = '{VERHASH}', charset = '{CHARSET}', discuz_uid = '$_G[uid]', cookiepre = '{$_G[config][cookie][cookiepre]}', cookiedomain = '{$_G[config][cookie][cookiedomain]}', cookiepath = '{$_G[config][cookie][cookiepath]}', showusercard = '{$_G[setting][showusercard]}', attackevasive = '{$_G[config][security][attackevasive]}', disallowfloat = '{$_G[setting][disallowfloat]}', creditnotice = '<!--{if $_G['setting']['creditnotice']}-->$_G['setting']['creditnames']<!--{/if}-->', defaultstyle = '$_G[style][defaultextstyle]', REPORTURL = '$_G[currenturl_encode]', SITEURL = '$_G[siteurl]', JSPATH = '$_G[setting][jspath]', CSSPATH = '$_G[setting][csspath]', DYNAMICURL = '{$_G[dynamicurl] or ''}';</script>
