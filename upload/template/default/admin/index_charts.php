6f40525fffef7835323f9d08381f861bfd7e8108
monkeye <monkeye@qq.com>
2025-10-15 12:54:04 +0800

    修改首页链接

--- upload/template/default/admin/index_charts.php
+++ upload/template/default/admin/index_charts.php
@@ -8,7 +8,7 @@
 		<div class="stat_box cl">
 			<div class="statitem ico01">{lang stats_main_members}
 				<em>{lang stats_main_today} {echo number_format($statvars['membersaddtoday'])}</em>
-				<span><a href="{ADMINSCRIPT}?action=members&operation=list" target="_blank">{echo number_format($statvars['members'])}</a></span>
+				<span><a href="{ADMINSCRIPT}?action=members&operation=search&submit=yes" target="_blank">{echo number_format($statvars['members'])}</a></span>
 			</div>
 			<div class="statitem ico02">{lang stats_main_onlinemembers}
 				<em>{lang stats_main_registers} {echo number_format($statvars['onlinemembers'])}</em>
@@ -16,7 +16,7 @@
 			</div>
 			<div class="statitem ico03">{lang stats_main_posters}
 				<em>{lang stats_main_ratio} $statvars['mempostpercent']%</em>
-				<span><a href="{ADMINSCRIPT}?action=members&operation=list&posts_low=1" target="_blank">{echo number_format($statvars['mempost'])}</a></span>
+				<span><a href="{ADMINSCRIPT}?action=members&operation=search&submit=yes&posts_low=1" target="_blank">{echo number_format($statvars['mempost'])}</a></span>
 			</div>
 			<div class="statitem ico04">{lang stats_main_posts}
 				<em>{lang stats_main_newposts} {echo number_format($statvars['postsaddtoday'])}</em>
