0eff8c384832f3922c0376d05e902bf7cdd11a6e
monkeye <monkeye@qq.com>
2026-03-24 17:29:47 +0800

    去除多余的注释

--- upload/source/i18n/TC_UTF8/block/lang_misctag.php
+++ upload/source/i18n/TC_UTF8/block/lang_misctag.php
@@ -6,12 +6,6 @@
  * https://license.discuz.vip
  */
 
-/**
- *      [Discuz!] (C)2001-2099 Comsenz Inc.
- *      This is NOT a freeware, use is subject to license terms
- *
- *      $Id: lang_adv.php 27449 2012-02-01 05:32:35Z zhangguosheng $
- */
 
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
