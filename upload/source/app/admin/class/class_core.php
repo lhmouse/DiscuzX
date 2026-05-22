4c47a25998c4dd4aa135a3f933b65a671f4addfc
DiscuzX <153619335@qq.com>
2026-05-22 01:04:15 +0000

    !2613 完善后台登录验证
    * 完善后台登录验证

--- upload/source/app/admin/class/class_core.php
+++ upload/source/app/admin/class/class_core.php
@@ -54,6 +54,10 @@ class class_core {
 		global $_G;
 
 		if($_G['config']['admincp']['validate']['method'] == 'default') {
+			if (empty($_SERVER['HTTP_USER_AGENT']) || empty($_G['config']['admincp']['validate']['user']) || empty($_G['config']['admincp']['validate']['user'])) {
+				header('HTTP/1.1 401 Unauthorized');
+				exit;
+			}
 			$realm = 'Discuz! Admincp';
 			$nonce = authcode(md5($_SERVER['HTTP_USER_AGENT'].$_G['cookie']['saltkey']),
 				'ENCODE', $_G['cookie']['saltkey'].substr(time(), 0, 6), 0, 0);
