3dd49d2a6c5549c89dd9ac4255a9bdb8597b21b8
monkeye <monkeye@qq.com>
2026-06-01 17:32:24 +0800

    修复 避免后台用户搜索因错误第三方数据白屏

--- upload/source/class/account/account_base.php
+++ upload/source/class/account/account_base.php
@@ -391,6 +391,9 @@ class account_base {
 	}
 
 	public static function getIcon($interface) {
+		if(empty($interface)) {
+			return ['', ''];
+		}
 		$icon = self::callClass($interface, 'icon', [], true);
 		if($icon) {
 			return [$icon, ''];
