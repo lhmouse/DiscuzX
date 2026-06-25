0e9ee5258bfab86c19f1289d550eea7751e8308b
zoewho <341936031@qq.com>
2026-06-25 11:31:12 +0800

    电脑端手机版预览页焕新升级

--- upload/template/default/touch/common/preview.php
+++ upload/template/default/touch/common/preview.php
@@ -1,25 +1,35 @@
 <?php exit('Access Denied');?>
 <!--{template common/header}-->
-<div id="ct" class="wp cl ptw pbw">
-	<table style="margin:0 auto"><tr><td>
-	<div class="z dzphone"><div>
-		<div class="scrtop">
-			<div class="cl"><div class="time"><!--{echo date('H:i')}--></div><div class="defect"></div></div>
-			<div class="siteurl"><!--{$_G['siteurl']}--></div>
-		</div>
-		<iframe id="ifm0" frameborder="0" src="misc.php?mod=mobile&view=true"></iframe>
-		<div class="homebar"></div>
-	</div></div>
-	<div class="z" style="margin-top: 60px; width: 430px;">
-		<div class="mtw bm bw0" style="background-color: #dfeaf4;">
-			<div class="bm_c">
-				<h1 class="xw1 xs2 mbn">{lang login_mobile}</h1>
-				<p class="mbw xg2">{lang login_mobile_join}</p>
-				<p class="hm mbw" style="font-size: 18px; color: #F60;">
-					<img src="data/cache/$file" />
-				</p>
+<div class="preview-page">
+	<div id="ct" class="wp cl ptw pbw">
+		<div class="preview-wrapper">
+			<div class="preview-phone-section">
+				<div class="dzphone"><div>
+						<div class="scrtop">
+							<div class="cl"><div class="time"><!--{echo date('H:i')}--></div></div>
+							<div class="siteurl"><!--{$_G['siteurl']}--></div>
+						</div>
+						<div class="phone-loading" id="phoneLoading">
+							<div class="spinner"></div>
+							<span>loading...</span>
+						</div>
+						<iframe id="ifm0" frameborder="0" src="misc.php?mod=mobile&view=true" onload="document.getElementById('phoneLoading').style.display='none';"></iframe>
+						<div class="homebar"></div>
+					</div></div>
+				<div class="phone-label">Mobile Preview</div>
+			</div>
+			<div class="preview-qr-section">
+				<div class="preview-qr-card">
+					<div class="card-icon"><img src="favicon.ico" alt="site icon" /></div>
+					<h1 class="xw1">{lang login_mobile}</h1>
+					<p class="subtitle">{lang login_mobile_join}</p>
+					<div class="qr-image">
+						<div class="scan-line"></div>
+						<img src="data/cache/$file" />
+					</div>
+				</div>
 			</div>
 		</div>
-	</div></td></tr></table>
+	</div>
 </div>
 <!--{template common/footer}-->
