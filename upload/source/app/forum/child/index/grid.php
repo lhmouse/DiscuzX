a2a73092ed1ce7b851feec68574877aac4325794
zoewho <341936031@qq.com>
2026-07-10 21:45:35 +0800

    修复 digest 线程数据的获取逻辑

--- upload/source/app/forum/child/index/grid.php
+++ upload/source/app/forum/child/index/grid.php
@@ -20,7 +20,6 @@ if(TIMESTAMP - $_G['cache']['grids']['cachetime'] < $cachelife) {
 	$_G['setting']['grid']['fids'] = in_array(0, $_G['setting']['grid']['fids']) ? 0 : $_G['setting']['grid']['fids'];
 
 	if($_G['setting']['grid']['gridtype']) {
-		$grids['digest'] = table_forum_thread::t()->fetch_all_for_guide('digest', 0, [], 3, 0, 0, 10, $_G['setting']['grid']['fids']);
 	} else {
 		$images = table_forum_threadimage::t()->fetch_all_order_by_tid(10);
 		foreach($images as $key => $value) {
@@ -32,6 +31,7 @@ if(TIMESTAMP - $_G['cache']['grids']['cachetime'] < $cachelife) {
 
 	$grids['newreply'] = table_forum_thread::t()->fetch_all_for_guide('reply', 0, [], 0, 0, 0, 10, $_G['setting']['grid']['fids']);
 	$grids['hot'] = table_forum_thread::t()->fetch_all_for_guide('hot', 0, [], 3, 0, 0, 10, $_G['setting']['grid']['fids']);
+	$grids['digest'] = table_forum_thread::t()->fetch_all_for_guide('digest', 0, [], 3, 0, 0, 10, $_G['setting']['grid']['fids']);
 
 	$_G['forum_colorarray'] = ['', '#EE1B2E', '#EE5023', '#996600', '#3C9D40', '#2897C5', '#2B65B7', '#8F2A90', '#EC1282'];
 	foreach($grids as $type => $gridthreads) {
